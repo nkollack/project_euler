6a95ab581cda5247237fb9cb42b2691f06dd3dd0
Neil Kollack <nkollack@gmail.com>
2022-08-07 20:49:18 -0500

    fix: update question print scheme

--- ProjectEuler/src/main/Question.java
+++ ProjectEuler/src/main/Question.java
@@ -1,18 +1,21 @@
 package main;
 
 public abstract class Question {
-	protected int questionNumber;
+	protected int q_num;
 	protected Question(int questionNumber) {
-		this.questionNumber = questionNumber;
+		this.q_num = questionNumber;
+	}
+	private String getQ_num() {
+		return String.format("%03d", q_num);
 	}
 	public abstract void execute();
 	protected void printAnswer() {
-		System.out.println("Q" + questionNumber + " Answer: INCOMPLETE");
+		System.out.println("Q" + getQ_num() + " Answer: INCOMPLETE");
 	}
 	protected void printAnswer(int answer) {
-		System.out.println("Q" + questionNumber + " Answer: " + answer);
+		System.out.println("Q" + getQ_num() + " Answer: " + answer);
 	}
 	protected void printAnswer(long answer) {
-		System.out.println("Q" + questionNumber + " Answer: " + answer);
+		System.out.println("Q" + getQ_num() + " Answer: " + answer);
 	}
 }
